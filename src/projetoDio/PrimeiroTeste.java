e91e3e54d5b382d5e53688da11849729f3e963e9
tamyrespereirasilva <tamyrestn88@gmail.com>
2023-02-28 17:46:04 -0300

    first commit

---
+++ src/projetoDio/PrimeiroTeste.java
@@ -0,0 +1,14 @@
+package projetoDio;
+
+import projetoDio.model.Gato;
+
+public class PrimeiroTeste {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+	}
+
+}
